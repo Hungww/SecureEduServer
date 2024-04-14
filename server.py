551ecbf9e99937a339a96b8051bd3b7bc3a49302
Hungww <luuchanhung123@gmail.com>
2024-04-14 08:08:53 +0700

    add dep

--- server.py
+++ server.py
@@ -111,7 +111,6 @@ def url_check():
 # %%
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
